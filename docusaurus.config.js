23f9cc4860b3613a09b733228359f6a87db70b12
Daniel Santos <danielcorreiadossantos10@gmail.com>
2025-05-16 11:10:31 +0100

    alteracoes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,20 +10,22 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Teste de texto',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://S1zzy18.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false, 
+  baseUrl: '/dokku/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'S1zzy18', // Usually your GitHub org/user name.
+  projectName: 'dokku', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
